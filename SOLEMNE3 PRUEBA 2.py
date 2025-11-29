2a860193afd999d9d3aa42007de6e4b5ac4cb0b4
DiegoMolinaGitgit <molinadiego968@gmail.com>
2025-11-29 20:33:15 -0300

    Update SOLEMNE3 PRUEBA 2.py

--- SOLEMNE3 PRUEBA 2.py
+++ SOLEMNE3 PRUEBA 2.py
@@ -120,15 +120,15 @@ with tab1:
         plt.tight_layout()
         st.pyplot(fig)
 
-    if selection == "Distribución de paises por Continentes" :
+ 
     
-        st.subheader("Distribución de paises por Continentes")
-        reg_counts = df["Región"].value_counts()
-    
-        fig, ax = plt.subplots(figsize=(2, 8))
-        pies = ax.pie(reg_counts.values, labels=reg_counts.index, textprops={'fontsize': 4}, autopct='%1.1f%%')
-        ax.set_title("Proporción de países por Continentes", fontdict={'fontsize': 15})
-        st.pyplot(fig)
+    st.subheader("Distribución de paises por Continentes")
+    reg_counts = df["Región"].value_counts()
+
+    fig, ax = plt.subplots(figsize=(2, 8))
+    pies = ax.pie(reg_counts.values, labels=reg_counts.index, textprops={'fontsize': 4}, autopct='%1.1f%%')
+    ax.set_title("Proporción de países por Continentes", fontdict={'fontsize': 15})
+    st.pyplot(fig)
 
 
     st.subheader("Relación entre área y población")
@@ -141,12 +141,10 @@ with tab1:
             color=mapa_colores[region],
             label=region
         )
-
     ax.set_xlabel("Área (km²)")
     ax.set_ylabel("Población")
     ax.set_title("Área vs Población de cada país por Continente")
     ax.legend(title="Continentes")
-
     plt.tight_layout()
     st.pyplot(fig)
 
@@ -159,6 +157,7 @@ with tab2:
         file_name="paises.csv",
         mime="text/csv"
     )
+
 
 
 
